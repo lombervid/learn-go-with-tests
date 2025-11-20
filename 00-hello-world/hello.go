1e6e0ef7107959ee214f8b24cf3fcf9f18d4467f
lombervid <david.lomberlan@gmail.com>
2025-11-20 15:09:33 -0600

    hello: add language option

--- 00-hello-world/hello.go
+++ 00-hello-world/hello.go
@@ -2,16 +2,37 @@ package main
 
 import "fmt"
 
-const englishHelloPrefix = "Hello, "
-const helloSuffix = "!"
+const (
+	spanish = "Spanish"
+	french = "French"
 
-func Hello(name string) string {
+	englishHelloPrefix = "Hello, "
+	spanishHelloPrefix = "Hola, "
+	frenchHelloPrefix = "Bonjour, "
+
+	helloSuffix = "!"
+)
+
+func Hello(name string, language string) string {
 	if name == "" {
 		name = "World"
 	}
-	return englishHelloPrefix + name + helloSuffix
+
+	return greetingPrefix(language) + name + helloSuffix
+}
+
+func greetingPrefix(language string) (prefix string) {
+	switch language {
+	case spanish:
+		prefix = spanishHelloPrefix
+	case french:
+		prefix = frenchHelloPrefix
+	default:
+		prefix = englishHelloPrefix
+	}
+	return
 }
 
 func main()  {
-	fmt.Println(Hello("world"))
+	fmt.Println(Hello("world", ""))
 }
