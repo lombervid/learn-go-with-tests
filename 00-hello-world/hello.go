3db6c3b22750d80ef177ac25ae67ee0294b58b73
lombervid <david.lomberlan@gmail.com>
2025-11-20 14:39:07 -0600

    hello: update code to return "Hello, World!" when an empty string is provided

--- 00-hello-world/hello.go
+++ 00-hello-world/hello.go
@@ -2,8 +2,14 @@ package main
 
 import "fmt"
 
+const englishHelloPrefix = "Hello, "
+const helloSuffix = "!"
+
 func Hello(name string) string {
-	return "Hello, " + name + "!"
+	if name == "" {
+		name = "World"
+	}
+	return englishHelloPrefix + name + helloSuffix
 }
 
 func main()  {
