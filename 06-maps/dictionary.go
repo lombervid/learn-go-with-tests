2bce75138e0b93e0ecea293d0fee207bfa480dc1
lombervid <david.lomberlan@gmail.com>
2025-12-03 14:50:10 -0600

    maps: add `Delete()` method

--- 06-maps/dictionary.go
+++ 06-maps/dictionary.go
@@ -49,3 +49,16 @@ func (d Dictionary) Update(word, definition string) error {
 	}
 	return nil
 }
+
+func (d Dictionary) Delete(word string) error {
+	_, err := d.Search(word)
+	switch err {
+	case ErrNotFount:
+		return ErrWordDoesNotExist
+	case nil:
+		delete(d, word)
+	default:
+		return err
+	}
+	return nil
+}
