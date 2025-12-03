aac43967239198feb8af00990d575a33d9318187
lombervid <david.lomberlan@gmail.com>
2025-12-03 13:33:34 -0600

    maps: add `Add()` method

--- 06-maps/dictionary_test.go
+++ 06-maps/dictionary_test.go
@@ -19,20 +19,38 @@ func TestSearch(t *testing.T) {
 		}
 		assertError(t, err, ErrNotFount)
 	})
+}
 
+func TestAdd(t *testing.T) {
+	dictionary := Dictionary{}
+	word := "test"
+	definition := "this is just a test"
+
+	dictionary.Add(word, definition)
+
+	assertDefinition(t, dictionary, word, definition)
+}
+
+func assertDefinition(t testing.TB, dictionary Dictionary, word, definition string) {
+	t.Helper()
+	got, err := dictionary.Search(word)
+	if err != nil {
+		t.Fatal("should find added word: ", err)
+	}
+	assertStrings(t, got, definition)
 }
 
 func assertStrings(t testing.TB, got, want string) {
 	t.Helper()
 	if got != want {
-		t.Errorf("got %q want %q given, %q", got, want, "test")
+		t.Errorf("got %q want %q given", got, want)
 	}
 }
 
 func assertError(t testing.TB, got, want error) {
 	t.Helper()
 	if got != want {
-		t.Errorf("got %q want %q given, %q", got, want, "test")
+		t.Errorf("got error %q want %q given", got, want)
 	}
 }
 func assertNoError(t testing.TB, got error) {
